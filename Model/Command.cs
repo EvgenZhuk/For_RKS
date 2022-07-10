1c6ea865de0bd636a3ad443be0cc0b690fb0e1c0
Евгений <deus_man@mail.ru>
2022-07-10 21:18:42 +0300

    Создан класс Command

---
+++ Model/Command.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_RKS.Model
+{
+    class Command
+    {
+        private string name;
+        private string code;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Code
+        {
+            get { return code; }
+            set
+            {
+                code = value;
+                OnPropertyChanged("Code");
+            }
+        }
+
+
+
+    }
+}
