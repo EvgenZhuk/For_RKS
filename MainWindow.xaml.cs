bc94c3ae674eaad5acdddde944ccb094927896da
Евгений <deus_man@mail.ru>
2022-07-10 23:00:45 +0300

    Реализовано чтение XML

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using For_RKS.ViewModel;
+using System.Xml;
+using For_RKS.Model;
 
 namespace For_RKS
 {
@@ -25,6 +27,31 @@ namespace For_RKS
         {
             InitializeComponent();
             DataContext = new ApplicationViewModel();
+
+
+            
+            var doc = new XmlDocument();
+            doc.Load("Commands.xml");
+            var element = doc.DocumentElement;
+            PrintItem(element);     
+            
+        }
+
+        private static void PrintItem(XmlElement element)
+        {
+            foreach (XmlNode xnode in element)
+            {
+                if (xnode.Attributes.Count > 0)
+                {
+                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
+                    XmlNode attr2 = xnode.Attributes.GetNamedItem("code");
+                    if (attr != null & attr2 != null)
+                    {
+                        //MessageBox.Show(attr.Value + "   " + attr2.Value);
+                    }
+                }
+            }
+
         }
     }
 }
